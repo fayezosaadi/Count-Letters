26faf9829ce23f67dbe7a19871b012a69027499a
Fayez O Saadi <fayezosaadi@gmail.com>
2017-04-05 22:30:59 +0000

    CharCount

--- charcounting.js
+++ charcounting.js
@@ -1,18 +1,19 @@
-var str = process.argv.slice(2).toString().toLowerCase().split(",").join("");
-console.log(str);
+var str = process.argv.slice(2).join("");
+// console.log(str);
 
 
 function countLetters(str) {
-    var freq = {};
-    for (var i=0; i<str.length;i++) {
-        var character = str.charAt(i);
-        if (freq[character]) {
-           freq[character]++;
-        } else {
-           freq[character] = 1;
-        }
+  var freq = {};
+  for (var i=0; i<str.length;i++) {
+    var character = str.charAt(i).toLowerCase();
+    if (character !== " "){
+    if (freq[character]) {
+      freq[character]++; }
+      else {
+        freq[character] = 1;
+      }
     }
-
-    return freq;
+  }
+  return freq;
 };
 console.log(countLetters(str));
